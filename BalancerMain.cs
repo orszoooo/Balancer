c6780b3a9e4e658e3a49a2c0a88fe6b74f975016
orszoooo <pawel.orszulik7@gmail.com>
2023-01-31 12:37:50 +0100

    Added GUI for setting the ball set point and displaying current set point.

--- BalancerMain.cs
+++ BalancerMain.cs
@@ -10,12 +10,12 @@ public class BalancerMain : Control
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        
+        Console.WriteLine("Hello!");
     }
 
 //  // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(float delta)
     {
-     
+
     }
 }
